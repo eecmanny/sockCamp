da6c7ba8e738bbbe343e7e2117930767554c2cbd
eecmanny <eec123manny@gmail.com>
2023-11-07 19:24:45 -0500

    models

--- server/models/questions.js
+++ server/models/questions.js
@@ -1,3 +1,32 @@
-const questions = [
-    
-]
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const questionSchema = new Schema({
+    questionText: {
+        type: String,
+        required: 'You need to provide a question!',
+        minLength: 1,
+        maxLength: 280,
+        trim: true,
+    },
+    questionTheme: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    questionAuthor: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    answers: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'Answer',
+        },
+    ],
+    });
+
+const Question = model('Question', questionSchema);
+
+module.exports = Question;
