040a2eb796db7ceccc51f7c5babfef797389eb40
Ulises <ulisesvivar123@gmail.com>
2023-11-09 20:42:39 -0500

    added the hook into the header.jsx so nav bar does not appear on home page

--- client/src/Header/Header.jsx
+++ client/src/Header/Header.jsx
@@ -1,48 +1,48 @@
-import  { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import "./header.css"
 
 function Header() {
-    // const location = useLocation();
+    const location = useLocation();
     // const navigate = useNavigate();
     return (
-        // {location.pathname !== '/Home' && (
-
         <header className="header">
-        
+
             <h1>SOCKCAMP</h1>
             <h2>Pre-Bootcamp</h2>
 
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/html">
-                            HTML
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/css">
-                            CSS
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/javascript">
-                            JavaScript
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/react">
-                            React
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/combo">
-                            Combo
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
+            {location.pathname !== '/' && (
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/html">
+                                HTML
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/css">
+                                CSS
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/javascript">
+                                JavaScript
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/react">
+                                React
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/combo">
+                                Combo
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>)}
         </header>
-        )}
+    )
+}
 //     );
 // }
 
