0aaaa86f1fa045bc045054b597ef0e957223f525
eecmanny <eec123manny@gmail.com>
2023-11-09 21:25:21 -0500

    questionsCase

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,6 @@ const Answer = require('./Answers');
 const Question = require('./Questions');
 const User = require('./User');
 
+//need to change to uppercase
+
 module.exports = { Answer, Question, User };
