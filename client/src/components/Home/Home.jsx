63600a7f33767bd9b36266f864d29dd5388fe150
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-11-09 19:42:29 -0500

    added starting code for each component page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -5,10 +5,24 @@ function Home() {
     return (
         <div className="Home">
             <h1>SOCKCAMP</h1>
-            <h2>Put on your sock before putting on your boot.</h2>
+            <h2>Pre-BootCamp</h2>
             <p>
-                TEXT FOR SOCKAMP DESCRIPTION GOES HERE
+                Because you must put your sock on, before you put your boot on.
             </p>
+            <div>
+                <button>
+                    <Link to="/">
+                        Login
+                    </Link>
+                </button>
+            </div>
+            <div>
+                <button>
+                    <Link to="/">
+                        Signup
+                    </Link>
+                </button>
+            </div>
         </div>
     );
 }
--- client/src/components/Javascript/Javascript.jsx
+++ client/src/components/Javascript/Javascript.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 const Javascript = () => {
   return (
-    <div>Javascript</div>
+    <div>
+      <div>
+        <h2>JavaScript</h2>
+        <p>JavaScript is ....</p>
+      </div>
+      <div>
+        <p>
+          Question 1: What does HTML stand for?
+        </p>
+      </div>
+      <div>
+        <p>
+          Answer: Hyper Text Markup Language
+        </p>
+      </div>
+      <div>
+        <p>
+          Question 2: What does HTML stand for?
+        </p>
+      </div>
+      <div>
+        <p>
+          Answer: Hyper Text Markup Language
+        </p>
+      </div>
+      <div>
+        <p>
+          Question 3: What does HTML stand for?
+        </p>
+      </div>
+      <div>
+        <p>
+          Answer: Hyper Text Markup Language
+        </p>
+      </div>
+    </div>
   )
 }
 
