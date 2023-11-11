b5860b455324e05c1ca36a05e3a764be9f86d2b5
Linda Vitrella <lvitrella@mail.com>
2023-11-11 14:21:33 -0500

    added comments to README and html css javascript and react jsx files

--- client/src/components/React/React.jsx
+++ client/src/components/React/React.jsx
@@ -6,7 +6,17 @@ const react = () => {
     <div className="react">
       <div className="react-container1">
         <h2>React</h2>
-        <p>React is...</p>
+        <p>React is.."React is a JavaScript library for building user interfaces."
+          It is maintained by Facebook and a community of individual developers and companies.
+          React is both a frontend and backend library.  It is used to build user interfaces and it is used to build the backend of a web site.
+          React is a component based library.  Components are independent and reusable bits of code.  They serve the same purpose as JavaScript 
+          functions, but work in isolation and return HTML via a render() function. 
+        
+          
+          
+          
+          
+          .</p>
       </div>
       <div className="react-container2">
         <div className="question">
