b70c38c4113dcc49b5a3098a9febb88ecf2182e2
eecmanny <eec123manny@gmail.com>
2023-11-12 19:20:57 -0500

    quizSetup_seedPopulate_css

--- client/src/components/Quiz/Quiz.jsx
+++ client/src/components/Quiz/Quiz.jsx
@@ -3,44 +3,89 @@ import './quiz.css'
 
 const Quiz = () => {
   return (
-    <div className="combo">
-      <div className="combo-container1">
-        <h2>Combo</h2>
-        <p>The Ultimate Test</p>
-      </div>
-      <div className="combo-container2">
-        <div className="question">
+
+
+    <main className='form'>
+
+<section>
+      <h2>Sock Camp - Quiz</h2>
+</section>
+
+<section className="quiz">
+<p className='quizContainer'>
+        <question className="questionCard">
           <p>
-            {/* {`Question 1: ${}`} */}
+                      Question 1: What does HTML stand for?
+          {/* Question 1: ${question} */}
+
           </p>
+
+        </question>
+
+        <div className='answerContainerCard'>
+          <answer className="answerPick">
+            <p>
+            Answer: Hyper Text Markup Language {/* {`answer 1: ${}`} */}
+            </p>
+          </answer>
+          <answer className="answerPick">
+            <p>
+              Answer: Hyper Text Markup Language {/* {`answer 1: ${}`} */}
+            </p>
+          </answer>
+          <answer className="answerPick">
+            <p>
+            Answer: Hyper Text Markup Language {/* {`answer 2: ${}`} */}
+            </p>
+          </answer>
+          <answer className="answerPick">
+            <p>
+              Answer: Hyper Text Markup Language {/* {`answer 1: ${}`} */}
+            </p>
+          </answer>
+          
         </div>
-        <div className="answer">
+
+      </p>
+
+      <p className='quizContainer'>
+        <question className="questionCard">
           <p>
-            Answer: Hyper Text Markup Language
+                      Question 1: What does HTML stand for?
+          {/* Question 1: ${question} */}
+
           </p>
+
+        </question>
+
+        <div className='answerContainerCard'>
+          <answer className="answerPick">
+            <p>
+            Answer: Hyper Text Markup Language {/* {`answer 1: ${}`} */}
+            </p>
+          </answer>
+          <answer className="answerPick">
+            <p>
+              Answer: Hyper Text Markup Language {/* {`answer 1: ${}`} */}
+            </p>
+          </answer>
+          <answer className="answerPick">
+            <p>
+            Answer: Hyper Text Markup Language {/* {`answer 2: ${}`} */}
+            </p>
+          </answer>
+          <answer className="answerPick">
+            <p>
+              Answer: Hyper Text Markup Language {/* {`answer 1: ${}`} */}
+            </p>
+          </answer>
+          
         </div>
-        <div className="question">
-          <p>
-          {/* {`Question 2: ${}`} */}
-          </p>
-        </div>
-        <div className="answer">
-          <p>
-            Answer: Hyper Text Markup Language
-          </p>
-        </div>
-        <div className="question">
-          <p>
-          {/* {`Question 3: ${}`} */}
-          </p>
-        </div>
-        <div className="answer">
-          <p>
-            Answer: Hyper Text Markup Language
-          </p>
-        </div>
-      </div>
-    </div>
+
+      </p>
+</section>
+
+    </main>
   )
 }
 
