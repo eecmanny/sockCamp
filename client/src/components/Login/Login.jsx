a6a3880368207783c5b8eadb318d912a9900053d
Ulises <ulisesvivar123@gmail.com>
2023-11-13 21:30:44 -0500

    updated the background image and have it added to signup and login page. adjusted header and footer to not be included on the welcome page. Brenny will adjust the signup and login page header / footer

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,49 +1,58 @@
 import { Link } from 'react-router-dom';
 import './login.css';
+import PageBackground from '../../antDcustoms/background.jsx';
 
 function Login() {
     return (
-        <form>
-            <div className="Login">
-                <h1>Login</h1>
-                <p>Happy Coding!</p>
-                <hr />
+        <div className="LoginPageBackground">
+            <div>
+                <PageBackground />
+                {/* <img src="./public/sockcampwelcome.jpg" alt="laptop background image"></img> */}
+            </div>
 
-                {/* <label htmlFor="email">
+            <form>
+                <div className="Login">
+                    <h1>Login</h1>
+                    <p>Happy Coding!</p>
+                    <hr />
+
+                    {/* <label htmlFor="email">
                     <b>Email</b>
                 </label>
                 <input type="text" placeholder="Enter Email" name="email" required /> */}
-<section className="loginCard">
-    <card className="loginBoxes">
+                    <section className="loginCard">
+                        <card className="loginBoxes">
                             <label htmlFor="username">
-                    <b>Username</b>
-                </label>
-                <input type="text" placeholder="Enter Username" name="username" required />
+                                <b>Username</b>
+                            </label>
+                            <input type="text" placeholder="Enter Username" name="username" required />
 
-                <label htmlFor="psw">
-                    <b>Password</b>
-                </label>
-                <input type="password" placeholder="Enter Password" name="psw" required />
+                            <label htmlFor="psw">
+                                <b>Password</b>
+                            </label>
+                            <input type="password" placeholder="Enter Password" name="psw" required />
 
-                <label>
-                    <input type="checkbox" checked="checked" name="remember" style={{ marginBottom: '15px' }} /> Remember me
-                </label>                    <div>
-                <Link to="/home">
-                <button className="button1">
-                        <h3>LOGIN</h3>
-                </button>
-                    </Link>
-                    <Link to="/signup">
-                <button className="button1">
-                        <h3>SIGNUP</h3>
-                </button>
-                    </Link>                    </div>
-    </card>
+                            <label>
+                                <input type="checkbox" checked="checked" name="remember" style={{ marginBottom: '15px' }} /> Remember me
+                            </label>                    <div>
+                                <Link to="/home">
+                                    <button className="button1">
+                                        <h3>LOGIN</h3>
+                                    </button>
+                                </Link>
+                                <Link to="/signup">
+                                    <button className="button1">
+                                        <h3>SIGNUP</h3>
+                                    </button>
+                                </Link>
+                            </div>
+                        </card>
 
-</section>
+                    </section>
 
-            </div>
-        </form>
+                </div>
+            </form>
+        </div>
     );
 }
 
