9ba030b22cfa4c5952d73517efc900f131e08299
eecmanny <eec123manny@gmail.com>
2023-11-14 21:02:35 -0500

    devs and Etc

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!){
+        addUser(username: $username, email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_SCORE = gql`
+    mutation addScore($score: Int!, $date: String!){
+        addScore(score: $score, date: $date){
+            _id
+            score
+            date
+        }
+    }
+`;
